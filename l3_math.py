c0d6850ef995fd968ccff7023a6fa402654fcff0
Strannik73 <Santon@tutamail.com>
2024-10-01 16:34:32 +0300

    yjv2

--- l3_math.py
+++ l3_math.py
@@ -1,3 +1,23 @@
+print ('радиус окружности в сантиметрах')
+r = int
+r = input()
 
-r = float(input('Введите радиус: '))
-p = 2 * 3,14 * r
+if int(r) >= 0:
+
+    c = int
+    c =  2*int(r)*3,14
+    print(str(c) + 'длина в сантиметрах')
+    cm = in /100
+    print(str(cm) + 'длина окружности в метрах')
+
+    so = int
+    so = 3,14*int(r)**2
+    print(str(so) + 'площадь в метрах')
+    som = int
+    som = int(som) / 100
+
+    a = int
+    a = (2*int(r))**0,5
+    
+
+ 
